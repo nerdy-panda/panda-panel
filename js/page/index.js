35676a510e69cdec08ebd9f8847c250a8d6f3b7b
Nerd Panda <nerdpandabusiness@gmail.com>
2024-10-11 13:33:40 +0200

    create

--- js/page/index.js
+++ js/page/index.js
@@ -1,1 +1,6 @@
-window.console.log('load index');
+import Accordion from "../Service/Accordion.js";
+
+const accordions = window.document.body.querySelectorAll(".sub-menu>a");
+for (const accordionElement of accordions) {
+    const accordion = new Accordion(accordionElement,"opened-menu");
+}
